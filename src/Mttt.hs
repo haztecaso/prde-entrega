f000e99b789556984343325f3f32f0d27962b199
Adrián Lattes <adrianlattes@disroot.org>
2020-11-24 13:11:46 +0100

    Siguiendo consejos de Fernando: mejor no usar newtype y definir los shows como funciones externas

--- src/Mttt.hs
+++ src/Mttt.hs
@@ -1,4 +1,4 @@
-{-|
+{-j
 Module      : Mttt
 Description : Meta tres en raya
 Copyright   : (c) Adrián Lattes y David Diez
@@ -8,159 +8,171 @@ Stability   : experimental
 Implementaciones de los juegos /tres en raya/ y /meta tres en raya/.
 -}
 
-module Mttt (
-  -- * Tipos
-    Ficha
-  , TableroP
-  , Tablero
-  -- * Algunos tableros
-  , tableroPVacio
-  , tableroVacio
-) where
+module Mttt where
+-- module Mttt (
+--   -- * Fichas, bloques y tableros
+--     Ficha
+--   , Bloque
+--   , Tablero
+--   -- ** Bloque y Tablero vacíos
+--   , bloqueVacio
+--   , tableroVacio
+--   -- * Jugando
+--   -- * Utilidades
+--   -- ** Convertir a strings
+--   , showBloque
+--   , showTablero
+--   -- ** Ejemplos 
+--   , ejemploBloque
+-- ) where
 
 import Data.Array
 import Data.List (intersperse, replicate, transpose)
 import Data.Maybe (isJust, isNothing)
 
 -- | Tipo que representa una ficha del juego
-data Ficha =  X -- ^ Cruz. Es la ficha que utiliza el primer jugador
-           | O  -- ^ Circulo. Ficha del segundo jugador
+data Ficha = X -- ^ Cruz. Ficha del primer jugador
+           | O -- ^ Circulo. Ficha del segundo jugador
     deriving (Eq, Read, Enum)
 
 instance Show Ficha where
     show X = "✗"
     show O = "○"
 
--- | Utilidad para imprimir una casilla del tablero en pantalla
-maybeFichaToChar :: Maybe Ficha -> Char
-maybeFichaToChar Nothing = '_'
-maybeFichaToChar (Just f)  = head (show f)
-
 -- | Tipo para un tablero de /tres en raya/
-newtype TableroP = Tp (Array (Int,Int) (Maybe Ficha))
-  deriving Eq 
-
-instance Show TableroP where
-  show (Tp arr) = unlines [intersperse ' ' [maybeFichaToChar $ arr!(x, y) | y <- [1..3]] | x <- [1..3]] 
+type Bloque = Array (Int,Int) (Maybe Ficha)
 
 -- | Tipo para un tablero de /meta tres en raya/
-newtype Tablero = T (Array (Int,Int) TableroP)
-  deriving Eq 
+type Tablero = Array (Int,Int) Bloque
 
-instance Show Tablero where
-  show _ = ""
-showTablero (T arr) = insertCol (1,1) col
-    where insertCol t = zipWith (++) $ map showLinea $ lineasP $ arr!t 
-          col = replicate 3 " |"
+-- | Utilidad para imprimir una /casilla/ de un 'Bloque' en pantalla
+showMaybeFicha :: Maybe Ficha -> Char
+showMaybeFicha Nothing = '_'
+showMaybeFicha (Just f)  = head (show f)
 
--- | Devuelve el array interno de un TableroP
-fromTableroP :: TableroP -> Array (Int, Int) (Maybe Ficha)
-fromTableroP (Tp arr) = arr
+-- | Representación en caracteres de un 'Bloque'
+showBloque :: Bloque -> String
+showBloque b = unlines [intersperse ' ' [showMaybeFicha $ b!(x, y) | y <- [1..3]] | x <- [1..3]] 
 
--- | Devuelve el array interno de un Tablero
-fromTablero :: Tablero -> Array (Int, Int) TableroP
-fromTablero (T arr) = arr
+-- | Utilidad para imprimir en pantalla un 'Bloque'
+putBloque :: Bloque -> IO ()
+putBloque = putStr . showBloque
 
--- | Lista de indices de un Tablero o TableroP
+-- | Representación en caracteres de un 'Tablero'
+showTablero :: Tablero -> String
+showTablero _ = ""
+
+-- | Utilidad para imprimir en pantalla un 'Tablero'
+putTablero :: Tablero-> IO ()
+putTablero = putStr . showTablero
+
+-- | Lista de indices de un Tablero o Bloque
 indicesTablero :: [(Int, Int)]
 indicesTablero = [(x, y) | x <- [1..3], y <- [1..3]]
 
--- | Tablero de tres en raya vacío
-tableroPVacio :: TableroP
-tableroPVacio = Tp (listArray ((1,1),(3,3)) [Nothing | _ <- indicesTablero])
+-- | 'Bloque' vacío
+bloqueVacio :: Bloque
+bloqueVacio = listArray ((1,1),(3,3)) [Nothing | _ <- indicesTablero]
 
--- | Tablero de meta tres en raya vacío
+-- | 'Tablero' vacío
 tableroVacio :: Tablero
-tableroVacio = T (listArray ((1,1),(3,3)) [tableroPVacio | _ <- indicesTablero])
+tableroVacio = listArray ((1,1),(3,3)) [bloqueVacio | _ <- indicesTablero]
 
--- | Insertar (o modificar) una ficha en un 'TableroP'
-ponerFichaP :: TableroP
-            -> (Int,Int) -- ^ Posición en la que se añade la ficha
+-- | Insertar una ficha nueva en un 'Bloque'.
+-- Si se intenta insertar una ficha en una casilla ya ocupada
+-- o no es el turno del jugador se lanzará un error.
+-- Las demás funciones deberían hacer un uso correcto, garantizando
+-- que nunca se lance el error.
+movimientoBloque ::
+            Bloque
             -> Ficha     -- ^ Ficha a añadir
-            -> TableroP
-ponerFichaP (Tp arr) (x,y) f = Tp (arr // [((x,y), Just f)])
+            -> (Int, Int) -- ^ Posición en la que se añade la ficha
+            -> Bloque
+movimientoBloque b f (x,y)
+  | (isNothing (b!(x,y))) && turnoBloque b == f = b // [((x,y), Just f)]
+  | otherwise            = error "Movimiento incorrecto"
 
--- | Lista de posiciones vacías de un 'TableroP'
-casillasLibresP :: TableroP -> [(Int, Int)]
-casillasLibresP (Tp arr) = map fst $ filter snd [((x, y), isNothing $ arr!(x,y)) | (x,y) <- indicesTablero]
+-- | Lista de posiciones vacías de un 'Bloque'
+casillasLibresBloque :: Bloque -> [(Int, Int)]
+casillasLibresBloque b = map fst $ filter snd [((x, y), isNothing $ b!(x,y)) | (x,y) <- indicesTablero]
 
 -- | Posibles jugadas
-expandirP :: TableroP -> [TableroP]
-expandirP t
-  | finP t    = []
-  | otherwise = map (\x -> ponerFichaP t x f) $ casillasLibresP t
-  where f = turnoP t
+expandirBloque :: Bloque -> [Bloque]
+expandirBloque t
+  | finBloque t = []
+  | otherwise   = map (movimientoBloque t f) $ casillasLibresBloque t
+  where f = turnoBloque t
 
 -- | Cuenta las fichas de cada tipo que hay en un tablero de tres en raya.
-contarFichasP :: TableroP
+contarFichasBloque :: Bloque
               -> (Int, Int) -- ^ El primer valor corresponde al número de X's y el segundo a las O's
-contarFichasP (Tp arr) = foldr1 suma $ map f $ elems arr
+contarFichasBloque b = foldr1 suma $ map f $ elems b
                        where f Nothing = (0,0)
                              f (Just X) = (1,0)
                              f (Just O) = (0,1)
                              suma (a,b) (c, d) = (a+c, b+d)
 
 -- | Devuelve las filas de un tablero de tres en raya
-filasP :: TableroP -> [[Maybe Ficha]]
-filasP (Tp arr) = [[arr!(x, y)| x <- [1..3]] | y <- [1..3]]
+filasBloque :: Bloque -> [[Maybe Ficha]]
+filasBloque b = [[b!(x, y)| x <- [1..3]] | y <- [1..3]]
 
 -- | Devuelve las columnas de un tablero de tres en raya
-columnasP :: TableroP -> [[Maybe Ficha]]
-columnasP = transpose . filasP
+columnasBloque :: Bloque -> [[Maybe Ficha]]
+columnasBloque = transpose . filasBloque
 
 -- | Devuelve las diagonales de un tablero de tres en raya
-diagonalesP :: TableroP -> [[Maybe Ficha]]
-diagonalesP (Tp arr) = [[arr!(x,x)| x<-[1..3]],[arr!(x, 4-x) |x<-[1..3]]]
+diagonalesBloque :: Bloque -> [[Maybe Ficha]]
+diagonalesBloque b = [[b!(x,x)| x<-[1..3]],[b!(x, 4-x) |x<-[1..3]]]
 
 -- | Devuelve todas las lineas rectas de un tablero de tres en raya
-lineasP :: TableroP -> [[Maybe Ficha]]
-lineasP = concat . sequence [filasP, columnasP, diagonalesP]
+lineasBloque :: Bloque-> [[Maybe Ficha]]
+lineasBloque = concat . sequence [filasBloque, columnasBloque, diagonalesBloque]
 -- | Determina a quien le toca
-turnoP :: TableroP -> Ficha
-turnoP t
+turnoBloque :: Bloque -> Ficha
+turnoBloque b
   | (xs - os) == 1 = O
   | (xs - os) == 0 = X
-  where (xs, os) = contarFichasP t
+  where (xs, os) = contarFichasBloque b
 
 -- | Determina quien ha ganado la partida de tres en raya, si es que alguien ha ganado.
-ganadorP :: TableroP -> Maybe Ficha
-ganadorP t
+ganadorBloque :: Bloque -> Maybe Ficha
+ganadorBloque t
   | Just X `elem` ganadores = Just X
   | Just O `elem` ganadores = Just O
   | otherwise = Nothing
-  where ganadores = map ganadorLinea $ lineasP t
+  where ganadores = map ganadorLinea $ lineasBloque t
         ganadorLinea l
           | l == [Just X, Just X, Just X] = Just X
           | l == [Just O, Just O, Just O] = Just O
           | otherwise = Nothing
 
 -- | Determina si la partida ha acabado o no
-finP :: TableroP -> Bool
-finP t
-  | isJust (ganadorP t) = True
-  | tablasP t           = True
-  | otherwise           = False
+finBloque :: Bloque -> Bool
+finBloque t
+  | isJust (ganadorBloque t) = True
+  | tablasBloque t           = True
+  | otherwise                = False
 
 
 -- | Determina si la partida de tres en raya ha acabado en tablas.
-tablasP :: TableroP -> Bool
-tablasP t@(Tp arr) = notElem Nothing arr && isNothing (ganadorP t)
+tablasBloque :: Bloque -> Bool
+tablasBloque b = notElem Nothing b && isNothing (ganadorBloque b)
 
 -- | Determina si una posición de un tablero de tres en raya es válida o no.
 --
 -- __TODO:__ Completar; Tener en cuenta que la partida debe acabar cuando alguien gana, por lo que no se puede ganar dos veces la partida.
-validarTableroP:: TableroP -> Bool
-validarTableroP t = (xs - os) == 0 || (xs - os) == 1
-    where (xs, os) = contarFichasP t
+validarBloque:: Bloque -> Bool
+validarBloque b = (xs - os) == 0 || (xs - os) == 1
+    where (xs, os) = contarFichasBloque b
 
 -- | Implementación del algoritmo minimax que recorre el árbol hasta las hojas.
--- minimaxPDeep :: ([Int] -> Int) -> ([a] -> a) -> TableroP -> a
--- minimaxPDeep peor mejor tablero
---   | finP tablero = heurPDeep tablero
---   | otherwise    = mejor (map (minimaxPDeep mejor peor) siguientes)
---   where siguientes = expandirP tablero
+-- minimaxBloqueDeep :: ([Int] -> Int) -> ([a] -> a) -> Bloque -> a
+-- minimaxBloqueDeep peor mejor tablero
+--   | finBloque tablero = heurBloque tablero
+--   | otherwise    = mejor (map (minimaxBloqueDeep mejor peor) siguientes)
+--   where siguientes = expandirBloque tablero
 
-minimaxPDeep = minimaxMain 10 expandirP heurP
+minimaxBloqueDeep = minimaxMain 10 expandirBloque heurBloque
 
 -- | Parte /interna/ del algoritmo minimax visto en clase
 minimax :: Ord b => Int -> (a->[a]) -> (a->b) -> ([b]->b) -> ([b]->b) -> a -> b
@@ -184,23 +196,23 @@ minimaxMain prof expandir evaluar prob
 
 -- | Función heurística para el tres en raya
 -- Toma el valor 0 si nadie ha ganado, 1 si ganan X y -1 si gana O.
-heurP :: Num a => TableroP -> a
-heurP t
+heurBloque :: Bloque -> Int
+heurBloque b
   | ganador == Just X = 1
   | ganador == Just O = -1
   | otherwise = 0
-  where ganador = ganadorP t
+  where ganador = ganadorBloque b
 
 -- | Utilidad para imprimir lineas de un tablero de tres en raya
 showLinea :: [Maybe Ficha] -> String
-showLinea = intersperse ' ' . map maybeFichaToChar
+showLinea = intersperse ' ' . map showMaybeFicha 
 
--- | Ejemplo de un posible tablero de tres en raya
-ejemploTableroP :: TableroP
-ejemploTableroP =  ponerFichaP (
-                     ponerFichaP (
-                       ponerFichaP (
-                         ponerFichaP tableroPVacio (2,2) X
-                       ) (1,1) O
-                     ) (2,1) X
-                   ) (3,3) O
+-- | Ejemplo de un posible 'Bloque'
+ejemploBloque :: Bloque
+ejemploBloque =  movimientoBloque (
+                     movimientoBloque (
+                       movimientoBloque (
+                         movimientoBloque bloqueVacio X (2,2)
+                       ) O (1,1)
+                     ) X (2,1)
+                   ) O (3,3)
