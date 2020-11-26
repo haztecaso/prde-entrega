ac62c29febd40d7d7914094f8f0d90e0c41e53c5
Adrián Lattes <adrianlattes@disroot.org>
2020-11-26 16:33:09 +0100

    cambios tontos

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,7 @@ module Main where
 
 import Mttt
 
-verBloque :: Bloque -> IO ()
-verBloque bloque = do
-  putBloque bloque
-  displayEB (crearEB bloque (0,0) 600 temaOscuro)
-
 main :: IO ()
 main = do
-  verBloque bloqueEjemplo
-  guiBoard temaOscuro 600
+  -- guiBoard temaOscuro 250
+  guiTablero temaOscuro 500
