fd361b84ba6bb034c1e3d46dcf75fc074016a052
Adrián Lattes <adrianlattes@disroot.org>
2020-11-26 05:00:24 +0100

    Continuando gui

--- src/Mttt/Gui.hs
+++ src/Mttt/Gui.hs
@@ -19,6 +19,7 @@ import Mttt.Utils
 import Mttt.Bloque
 import Mttt.Tablero
 
+import Data.Array
 import Graphics.Gloss
 import Graphics.Gloss.Data.Color
 import Graphics.Gloss.Interface.IO.Interact
@@ -51,36 +52,6 @@ temaOscuro = Tema { fondo     = greyN 0.15
                  , neutro     = greyN 0.4
                  }
 
--- | Dibujar una cruz
-dibujaX :: Point -- ^ Posición del centro de la X
-        -> Float -- ^ Tamaño de la X
-        -> Float -- ^ Grosor de la X
-        -> Color -- ^ Color del círculo
-        -> Picture
-dibujaX pos tam gros color = translate x y $ pictures $ zipWith rotate [45.0,-45.0] [rect, rect]
-                               where rect   = rectangleSolid (sqrt 2 * tam)  gros
-                                     (x, y) = pos
--- | Dibujar un círculo
-dibujaO :: Point -- ^ Posición del centro del círculo
-        -> Float -- ^ Tamaño del círculo
-        -> Float -- ^ Grosor del círculo
-        -> Color -- ^ Color del círculo
-        -> Picture
-dibujaO pos tam gros color = translate x y $ pictures $ zipWith rotate [45.0,-45.0] [rect, rect]
-                               where rect   = rectangleSolid (sqrt 2 * tam)  gros
-                                     (x, y) = pos
-
--- | Dibujar una ficha
-dibujaFicha :: Tema  -- ^ Tema con el que dibujar la 'Ficha'
-            -> Float -- ^ Tamaño de la ficha
-            -> Point -- ^ Posición de la ficha (esquina superior izquierda)
-            -> Ficha -- ^ Ficha a dibujar
-            -> Picture
-dibujaFicha tema tam pos ficha
-  | esX ficha = dibujaX centro tam (tam*0.15) (principal tema)
-  | otherwise = dibujaO centro tam (tam*0.15) (secundario tema)
-  where centro = (fst pos + tam/2, snd pos + tam/2)
-
 -- | Dibujar lineas del tablero en un cuadrado
 dibujaLineas :: Point -- ^ Esquina superior izquierda del tablero
              -> Float -- ^ Tamaño del tablero
@@ -89,28 +60,87 @@ dibujaLineas :: Point -- ^ Esquina superior izquierda del tablero
 dibujaLineas p t c = color c $ pictures $
   [ line [(x, 0),(x, t)] | x <- pos]
   ++ [ line [(0, y),(t, y)] | y <- pos]
-      where pos = map (+ fst p) [t/3, t*2/3]
-            linea = 
+      where pos   = map (+ fst p) [t/3, t*2/3]
+
+-- | Dibujar una cruz
+dibujaX :: Point -- ^ Posición del centro de la X
+        -> Float -- ^ Tamaño de la X
+        -> Float -- ^ Grosor de la X
+        -> Color -- ^ Color del círculo
+        -> Picture
+dibujaX pos tam gros col = translate x y $ color col $ pictures $ zipWith rotate [45.0,-45.0] [rect, rect]
+                               where rect   = rectangleSolid (sqrt 2 * (tam - gros))  gros
+                                     (x, y) = pos
+-- | Dibujar un círculo
+dibujaO :: Point -- ^ Posición del centro del círculo
+        -> Float -- ^ Tamaño del círculo
+        -> Float -- ^ Grosor del círculo
+        -> Color -- ^ Color del círculo
+        -> Picture
+dibujaO pos tam gros col = translate x y $ color col $ thickCircle ((tam - gros)/2)  gros
+                               where (x, y) = pos
+
+-- | Dibujar una ficha
+dibujaFicha :: Tema  -- ^ Tema con el que dibujar la 'Ficha'
+            -> Float -- ^ Tamaño de la ficha
+            -> Point -- ^ Posición de la ficha (esquina inferior izquierda)
+            -> Ficha -- ^ Ficha a dibujar
+            -> Picture
+dibujaFicha tema tam pos ficha
+  | esX ficha = dibujaX centro tam (tam*0.15) (principal tema)
+  | otherwise = dibujaO centro tam (tam*0.15) (secundario tema)
+  where centro = (fst pos + tam/2, snd pos + tam/2)
+
+dibujaMaybeFicha :: Tema  -- ^ Tema con el que dibujar la 'Ficha'
+            -> Float -- ^ Tamaño de la ficha
+            -> Point -- ^ Posición de la ficha (esquina inferior izquierda)
+            -> Maybe Ficha -- ^ Casilla a dibujar
+            -> Picture
+dibujaMaybeFicha tema tam pos (Just ficha) = dibujaFicha tema tam pos ficha
+dibujaMaybeFicha _ _ _ Nothing = Blank
+
+-- | Convertir de 'Pos' a posición 'Point'
+-- TODO: explicar mejor
+posicionFicha :: Float -- ^ Tamaño de la ficha
+              -> Pos   -- ^ Posición de la ficha
+              -> Point -- ^ Posición del centro de la ficha en el dibujo
+posicionFicha tam pos = (tam*(y-1), tam*(3-x))
+  where (x, y) =(fromIntegral $ fst pos, fromIntegral $ snd pos)
+
+-- | Dibujar una casilla del 'Bloque'
+dibujaCasilla :: Tema
+              -> Float -- ^ Tamaño del tablero
+              -> Bloque
+              -> Pos
+              -> Picture
+dibujaCasilla tema tam bloque pos = dibujaMaybeFicha tema (tam/3) origen casilla
+  where casilla = bloque!pos
+        origen = posicionFicha (tam/3) pos
 
 -- | Dibujar un bloque
 dibujaBloque :: Tema -- ^ Tema con el que dibujar el 'Bloque'
              -> Float -- ^ Tamaño del tablero
              -> Bloque
              -> Picture
-dibujaBloque tema tam _ = pictures [ dibujaLineas (0,0) tam (contraste tema)
-                                   , dibujaFicha tema (tam/3) (0,0) X
-                                   ]
+dibujaBloque tema tam bloque = translate (-tam/2) (-tam/2) $ pictures $
+                               [dibujaLineas (0,0) tam (contraste tema)] ++
+                               [dibujaCasilla tema tam bloque pos | pos <- listaIndices]
 
--- | Dibuja la celda de un 'Bloque' correspondiente a una 'Pos'ición.
--- dibujaPosBloque :: Bloque -> Pos -> Picture
--- dibujaPosBloque b p = 
+
 
 -- | TODO
 modificarBloque :: Event -> Bloque -> Bloque
 modificarBloque _ b = b
 
-bloqueVentana = InWindow "Tres en raya" (640, 480) (100, 100)
+bloqueVentana :: Display
+bloqueVentana = InWindow "Tres en raya" (800, 800) (0,0)
 
+-- | Función IO para pintar un 'Bloque' en pantalla.
+-- Tiene la misma interfaz que 'dibujaBloque'
+displayBloque :: Tema -> Float -> Bloque -> IO ()
+displayBloque tema tam bloque = display bloqueVentana (fondo tema) (dibujaBloque tema tam bloque)
+
+-- | Función IO para jugar al tres en raya
 guiBoard :: Tema  -- ^ Tema con el que dibujar la interfaz
          -> Float -- ^ Tamaño del tablero
          -> IO ()
