04afae32465e40a4b3aae9250695fef21eee8e8a
Adrián Lattes <adrianlattes@disroot.org>
2020-12-10 11:33:38 +0100

    Implementación de lineasTablero, ganadorTablero y turnoTablero. Empezando movTablero.

--- src/Mttt/Tablero/Data.hs
+++ src/Mttt/Tablero/Data.hs
@@ -10,16 +10,19 @@ Implementación del juego /meta tres en raya/.
 module Mttt.Tablero.Data (
      Tablero(bloques, bloqueActivo)
    , showTablero
-   , showTablero'
    , putTablero
    , tableroVacio 
+   , turnoTablero
+   , ganadorTablero
+                         , lineasTablero
 ) where
 
 import Mttt.Common.Utils
 import Mttt.Bloque.Data
 
 import Data.List (transpose, intersperse)
-import Data.Array
+import Data.Array (Array, (!), (//), listArray, elems)
+import Data.Maybe (isJust, isNothing, fromJust)
 
 -- | Tipo para un tablero de /meta tres en raya/
 data Tablero = T { bloques :: Array Pos Bloque -- ^ Bloques del tablero
@@ -27,7 +30,6 @@ data Tablero = T { bloques :: Array Pos Bloque -- ^ Bloques del tablero
                                                -- Si es 'Nothing' se puede jugar en
                                                -- cualquier 'Bloque'.
                  } deriving (Eq, Show, Read)
-
 
 -- | Representación en caracteres de un 'Tablero'
 showTablero :: Tablero -> String
@@ -51,3 +53,52 @@ tableroVacio =
   T { bloques = listArray ((1,1),(3,3)) [bloqueVacio | _ <- listaIndices]
     , bloqueActivo = Nothing 
     }
+
+-- | Cuenta las fichas de cada tipo que hay en un 'Tablero'.
+contarFichasTablero :: Tablero
+              -> (Int, Int) -- ^ El primer valor corresponde al número de X's y el segundo a las O's
+contarFichasTablero t = foldr1 suma [ contarFichasBloque $ (bloques t) ! i | i <- listaIndices]
+                       where suma (a,b) (c, d) = (a+c, b+d)
+
+-- | Devuelve todas las lineas rectas de un 'Tablero'
+lineasTablero :: Tablero -> [[Bloque]]
+lineasTablero t = filas ++ columnas ++ diagonales
+  where filas      = [[(bloques t)!(x, y)| x <- [1..3]] | y <- [1..3]]
+        columnas   = transpose filas
+        diagonales = [[(bloques t)!(x,x)| x<-[1..3]],[(bloques t)!(x, 4-x) |x<-[1..3]]]
+
+-- | Determina quien ha ganado la partida si es que alguien ha ganado.
+ganadorTablero :: Tablero -> Maybe Ficha
+ganadorTablero t
+  | Just X `elem` ganadores = Just X
+  | Just O `elem` ganadores = Just O
+  | otherwise = Nothing
+    where ganadores = map ganadorLinea $ lineasTablero t
+          ganadorLinea l
+            | map ganadorBloque l == [Just X, Just X, Just X] = Just X
+            | map ganadorBloque l == [Just O, Just O, Just O] = Just O
+            | otherwise = Nothing
+
+-- | Determina a quien le toca
+turnoTablero :: Tablero -> Maybe Ficha
+turnoTablero t
+  | (isNothing $ ganadorTablero t) && (xs - os) == 1 = Just O
+  | (isNothing $ ganadorTablero t) && (xs - os) == 0 = Just X
+  | otherwise = Nothing
+  where (xs, os) = contarFichasTablero t
+
+-- | Insertar una ficha nueva en un 'Tablero'. La función hace uso de
+-- 'turnoTablero' para decidir que 'Ficha' colocar.
+--
+-- Si el movimiento es válido se devuelve un 'Just Tablero'.
+-- En caso contrario se devuelve 'Nothing' 
+    {- TODO
+movTablero :: Tablero
+           -> Pos   -- ^ 'Bloque' en que jugar
+           -> Pos   -- ^ Posición del 'Bloque' seleccionado donde poner la ficha
+           -> Maybe Tablero
+movTablero t p1 p2
+  | Just p1 == bloqueActivo t = 
+  | otherwise = error "F"
+  where bloque = 
+-}
